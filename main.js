5fc871b5d872db72a2cad09193e814a61efcbdba
Ramki29 <ramkie7010@gmail.com>
2023-04-14 11:18:21 +0530

    add findHouse

---
+++ main.js
@@ -0,0 +1,28 @@
+var owl = $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+
+
+
+let toggleBtn = document.querySelector('#nav-bar');
+function btn() {
+    toggleBtn.classList.toggle('head-logos');
+}
